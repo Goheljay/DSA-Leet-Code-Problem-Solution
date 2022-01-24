42bfdb143be060ebd83089760ab656b2bd131ad5
firstproject <email>
2022-01-24 10:54:25 +0530

    DSA Practice

--- leetCode/easy/Searching/CountNagativeinSortedMatrix.java
+++ leetCode/easy/Searching/CountNagativeinSortedMatrix.java
@@ -1,4 +1,4 @@
-package com.jay.leetCode.easy;
+package com.jay.leetCode.easy.Searching;
 
 public class CountNagativeinSortedMatrix {
     public static void main(String[] args) {
@@ -6,12 +6,13 @@ public class CountNagativeinSortedMatrix {
     }
     public int countNegatives(int[][] grid) {
         int count = 0;
-        for(int[] g : grid) {
+        for (int k = 0; k < grid.length; k++) {
+            int[] g = grid[k];
             int i = 0;
             int j = g.length - 1;
-            while(i <= j) {
-                int mid = (i + j)/2;
-                if(g[mid] >= 0) {
+            while (i <= j) {
+                int mid = (i + j) / 2;
+                if (g[mid] >= 0) {
                     i = mid + 1;
                 } else {
                     j = mid - 1;
